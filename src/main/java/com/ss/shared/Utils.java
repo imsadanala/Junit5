0ce58e4155a3811f69a6b8c7ece93d7dc2c4b6c0
Sadanala <ssadanala@deloitte.com>
2021-02-24 09:45:18 +0530

    life cycle methods

---
+++ src/main/java/com/ss/shared/Utils.java
@@ -0,0 +1,9 @@
+package com.ss.shared;
+
+public class Utils {
+
+	public static Integer add(Integer p, Integer q) {
+		return p + q;
+	}
+
+}
--- src/test/java/com/ss/JunitTddApplicationTests.java
+++ src/test/java/com/ss/JunitTddApplicationTests.java
@@ -1,13 +1,47 @@
 package com.ss;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ss.shared.Utils;
 
 @SpringBootTest
 class JunitTddApplicationTests {
 
+	@BeforeAll
+	public static void setUp() {
+		System.out.println("@BeforeAll");
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		System.out.println("@AfterAll");
+	}
+
+	@BeforeEach
+	public void setUpRecc() {
+		System.out.println("@BeforeEach Recc");
+	}
+
+	@AfterEach
+	public void tearDownRecuu() {
+		System.out.println("@BeforeEach Recc");
+	}
+
 	@Test
-	void contextLoads() {
+	public void testAdd() {
+		assertEquals(10, Utils.add(5, 5));
+	}
+
+	@Test
+	public void testAddDiff() {
+		assertEquals(0, Utils.add(0, 0));
 	}
 
 }
