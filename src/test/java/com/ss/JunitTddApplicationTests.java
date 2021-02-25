6879a3df6e63255dfbb30eb75297af04b4e32fe7
Sadanala <ssadanala@deloitte.com>
2021-02-25 08:24:30 +0530

    @DisplayName annotations changes

--- src/test/java/com/ss/JunitTddApplicationTests.java
+++ src/test/java/com/ss/JunitTddApplicationTests.java
@@ -6,42 +6,52 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ss.shared.Utils;
 
+@DisplayName(value = "All test cases of Utils")
 @SpringBootTest
 class JunitTddApplicationTests {
 
 	@BeforeAll
 	public static void setUp() {
-		System.out.println("@BeforeAll");
-	}
-
-	@AfterAll
-	public static void tearDown() {
-		System.out.println("@AfterAll");
+		System.out.println("@BeforeAll...");
 	}
 
 	@BeforeEach
-	public void setUpRecc() {
-		System.out.println("@BeforeEach Recc");
-	}
-
-	@AfterEach
-	public void tearDownRecuu() {
-		System.out.println("@BeforeEach Recc");
+	public void setUpEach() {
+		System.out.println("@BeforeEach...");
 	}
 
 	@Test
-	public void testAdd() {
+	@DisplayName(value = "positive numbers test")
+	public void testAdd_Positive_Numbers() {
 		assertEquals(10, Utils.add(5, 5));
 	}
 
 	@Test
-	public void testAddDiff() {
-		assertEquals(0, Utils.add(0, 0));
+	@DisplayName(value = "negative numbers test")
+	public void testAdd_Negative_numbers() {
+		assertEquals(-4, Utils.add(-1, -3));
+	}
+
+	@Test
+	@DisplayName(value = "both positive and negative numbers test")
+	public void testAdd_One_Positive_One_Negative() {
+		assertEquals(20, Utils.add(30, -10));
+	}
+
+	@AfterEach
+	public void tearDownEach() {
+		System.out.println("@AfterEach...");
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		System.out.println("@AfterAll...");
 	}
 
 }
