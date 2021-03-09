8e8281cea68a96016008de48167fb99b83ab9323
imsadanala <imsadanala@imsadanala-PC>
2021-03-09 23:16:47 +0530

    @Disabled changes

--- src/test/java/com/ss/TddApplicationTests.java
+++ src/test/java/com/ss/TddApplicationTests.java
@@ -6,12 +6,16 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ss.shared.Utils;
 
+
+
+@Disabled(value = "Disabled all tests under this class to fix Bug#3456")
 @DisplayName(value = "All test cases of Utils")
 @SpringBootTest
 class TddApplicationTests {
@@ -27,19 +31,19 @@ class TddApplicationTests {
 	}
 
 	@Test
-	@DisplayName(value = "positive numbers test")
+	// @DisplayName(value = "positive numbers test")
 	public void testAdd_Positive_Numbers() {
 		assertEquals(10, Utils.add(5, 5));
 	}
 
 	@Test
-	@DisplayName(value = "negative numbers test")
+	// @DisplayName(value = "negative numbers test")
 	public void testAdd_Negative_numbers() {
 		assertEquals(-4, Utils.add(-1, -3));
 	}
 
 	@Test
-	@DisplayName(value = "both positive and negative numbers test")
+	// @DisplayName(value = "both positive and negative numbers test")
 	public void testAdd_One_Positive_One_Negative() {
 		assertEquals(20, Utils.add(30, -10));
 	}
